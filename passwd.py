03c72bea9ba819405501ebb4216651b8f4cdefb3
MateusFrancelino <32208103+MateusFrancelino@users.noreply.github.com>
2019-10-23 20:56:29 -0300

    Update passwd.py

--- passwd.py
+++ passwd.py
@@ -1,5 +1,6 @@
 def criauser(usuario,id,id2,name,home,shell):
-    usuario.append(input("Digite o Nome de usuario"))
+    nome=input("Digite o Nome de usuario")
+    usuario.append("\n"+nome)
     id.append(input("Digite o ID"))
     id2.append(input("Digite o ID de grupo"))
     name.append(input("Digite o Nome"))
@@ -23,12 +24,12 @@ def deletauser(usuario,id,id2,name,home,shell):
 
 def cria_arquivo(usuario,id,id2,name,home,shell):
     cont = 0
-    novoarquivo = open("novoarquivo.txt","w")
+    novopass = open("novopass.txt","w")
     for aux in usuario:
-        line = (usuario[cont]+":x:"+id[cont]+":"+id2[cont]+":"+name[cont]+":"+home[cont]+":"+shell[cont]+"\n")
-        novoarquivo.write(line)
+        line = (usuario[cont]+":x:"+id[cont]+":"+id2[cont]+":"+name[cont]+":"+home[cont]+":"+shell[cont])
+        novopass.write(line)
         cont += 1
-    novoarquivo.close()
+    novopass.close()
 def mostrar(usuario,id,id2,name,home,shell):
     n = len(usuario)
     for aux in range(0,n):
@@ -50,24 +51,17 @@ def moduser(usuario,id,id2,name,home,shell):
 
 
 
-
-
-
-
 passwd=open('passwd.txt')
 linha=passwd.readlines()
 passwd.close()
 entrada=True
 
-
 usuario=[]
-senha='x'
 id=[]
 id2=[]
 name=[]
 home=[]
 shell=[]
-i=0
 for linha in linha:
     dados=linha.split(":")
     usuario.append(dados[0])
@@ -76,11 +70,6 @@ for linha in linha:
     name.append(dados[4])
     home.append(dados[5])
     shell.append(dados[6])
-    print (dados[0],":",dados[1],":",dados[2],":",dados[3],":",dados[4],":",dados[5])
-
-
-
-
 
 while(entrada==True):
     print("1-Para criar novo usario")
@@ -88,20 +77,22 @@ while(entrada==True):
     print("3-Para editar um usuario")
     print("4-Para Salvar Arquivo")
     print("5-Para Mostrar Conteudo")
-    print("5-Para sair")
+    print("6-Para Sair")
     select=int(input())
-    if(select==1):
+    if(select == 1):
         criauser(usuario,id,id2,name,home,shell)
-    elif(select==2):
+    elif(select == 2):
         deletauser(usuario,id,id2,name,home,shell)
-    elif(select==3):
+    elif(select == 3):
         moduser(usuario,id,id2,name,home,shell)
+    elif(select == 4):
+        cria_arquivo(usuario,id,id2,name,home,shell)
     elif (select == 5):
         mostrar(usuario, id, id2, name, home, shell, )
 
-    elif(select==4):
-        cria_arquivo(usuario,id,id2,name,home,shell)
-    else:
+    elif(select == 6):
         entrada=False
+    else:
+        print("Opção invalida")
 
 
